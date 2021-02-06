606fe0b68a1aaf4c68bfb296de0d0b7b4d5c83d5
BIKRAM BISWAS <22101997>
2021-02-06 20:48:51 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<climits>
+#include<cfloat>
+#include<unordered_set>
+#include <unordered_map>
+#include<string>
+#include <cstring>
+#include<stack>
+#include<queue>
+#include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <cstdlib>
+#include<cassert>
+#define endl '\n'
+typedef long long int ll;
+typedef unsigned long long int ull;
+#define MOD 1000000007
+#define PINF LLONG_MAX
+#define NINF LLONG_MIN
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+using namespace std;
+int main()
+{
+    IOS;
+    
+    
+    return 0;
+}
